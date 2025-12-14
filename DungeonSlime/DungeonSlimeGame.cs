41a7a86f2184ecc18be75d2dfe570d99127ba18c
Kevin Chesser <kevin.chesser94@gmail.com>
2025-12-15 00:44:10 +0900

    add texture atlas, load sprites via xml config into texture atlas

--- DungeonSlime/DungeonSlimeGame.cs
+++ DungeonSlime/DungeonSlimeGame.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGameLibrary;
+using MonoGameLibrary.Graphics;
 using Vector2 = Microsoft.Xna.Framework.Vector2;
 
 namespace DungeonSlime
@@ -13,7 +14,8 @@ namespace DungeonSlime
         private static readonly int WindowWidth = 1280;
         private static readonly int WindowHeight = 720;
         private static readonly bool FullscreenDefault = false;
-        private Texture2D _logo;
+        private TextureRegion _slime;
+        private TextureRegion _bat;
 
         //Core systems
         public DungeonSlimeGame() : base(GameTitle, WindowWidth, WindowHeight, FullscreenDefault)
@@ -35,7 +37,9 @@ namespace DungeonSlime
         protected override void LoadContent()
         {
             // TODO: use this.Content to load your game content here
-            _logo = Content.Load<Texture2D>("images/logo");
+            TextureAtlas textureAtlas = TextureAtlas.FromFile(Content, "images/atlas-definition.xml");
+            _slime = textureAtlas.GetRegion("slime");
+            _bat = textureAtlas.GetRegion("bat");
             base.LoadContent();
         }
 
@@ -52,23 +56,16 @@ namespace DungeonSlime
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.DeepSkyBlue);
-            SpriteBatch.Begin();
-            SpriteBatch.Draw(
-                _logo,
-                new Vector2(
-                    (Window.ClientBounds.Width * 0.5f) - (_logo.Width * 0.5f),
-                    (Window.ClientBounds.Height * 0.5f) - (_logo.Height * 0.5f)
-                    ),
-                 null,               //sourceRectangle -  	An optional region within the texture to be rendered in order to draw only a portion of the texture. Specifying null will render the entire texture.
-                 Color.White,        //Color - The color mask (tint) to apply to the image drawn. Specifying Color.White will render the texture with no tint.
-                 0.0f,               //Rotation -  	The amount of rotation, in radians, to apply to the texture when rendering. Specifying 0.0f will render the image with no rotation.
-                 Vector2.Zero,       //Origin - The X and Y coordinates defining the position of the texture origin. This affects the texture's offset during rendering and serves as a reference point around which the texture is rotated and scaled.
-                 1.0f,               //Scale - The scale factor applied to the image across the x- and y-axes. Specifying 1.0f will render the image at its original size with no scaling.
-                 SpriteEffects.None, //Effects - A SpriteEffects enum value that specifies if the texture should be rendered flipped across the horizontal axis, the vertical axis, or both axes.
-                 0.0f);              //LayerDepth - Specifies the depth at which the texture is rendered. Textures with a higher layer depth value are drawn on top of those with a lower layer depth value. Note: This value will only apply when using SpriteSortMode.FrontToBack or SpriteSortMode.BackToFront.
-            SpriteBatch.End();
 
-            // TODO: Add your drawing code here
+            SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
+
+            // Draw the slime texture region at a scale of 4.0
+            _slime.Draw(SpriteBatch, Vector2.Zero, Color.White, 0.0f, Vector2.One, 4.0f, SpriteEffects.None, 0.0f);
+
+            // Draw the bat texture region 10px to the right of the slime at a scale of 4.0
+            _bat.Draw(SpriteBatch, new Vector2(_slime.Width * 4.0f + 10, 0), Color.White, 0.0f, Vector2.One, 4.0f, SpriteEffects.None, 1.0f);
+
+            SpriteBatch.End();
 
             base.Draw(gameTime);
         }
--- MonoGameLibrary/Core.cs
+++ MonoGameLibrary/Core.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using MonoGameLibrary.Graphics;
 
 namespace MonoGameLibrary
 {
