5ffe234cb576c6f742a0569b3761b81fb4d4ca9e
Kevin Chesser <kevin.chesser94@gmail.com>
2025-12-15 14:45:48 +0900

    implement collision detection with circles

--- DungeonSlime/DungeonSlimeGame.cs
+++ DungeonSlime/DungeonSlimeGame.cs
@@ -24,6 +24,9 @@ namespace DungeonSlime
 
         private const float MOVEMENT_SPEED = 5.0f;
 
+        private Vector2 _batPosition;
+        private Vector2 _batVelocity;
+
         //Core systems
         public DungeonSlimeGame() : base(GameTitle, WindowWidth, WindowHeight, FullscreenDefault)
         {
@@ -36,6 +39,9 @@ namespace DungeonSlime
             // TODO: Add your initialization logic here
 
             base.Initialize(); //Should never be removed - Graphics device is initialized
+
+            _batPosition = new Vector2(_slime.Width + 10, 0);
+            AssignRandomBatVelocity();
         }
 
         //Load textures, music, sound effects, misc game assets.
@@ -67,6 +73,109 @@ namespace DungeonSlime
             //Check for gamepad input and handle it
             CheckGamePadInput();
 
+            // Create a bounding rectangle for the screen.
+            Rectangle screenBounds = new Rectangle(
+                0,
+                0,
+                GraphicsDevice.PresentationParameters.BackBufferWidth,
+                GraphicsDevice.PresentationParameters.BackBufferHeight
+            );
+
+            // Creating a bounding circle for the slime
+            Circle slimeBounds = new Circle(
+                (int)(_slimePosition.X + (_slime.Width * 0.5f)),
+                (int)(_slimePosition.Y + (_slime.Height * 0.5f)),
+                (int)(_slime.Width * 0.5f)
+            );
+
+            // Use distance based checks to determine if the slime is within the
+            // bounds of the game screen, and if it is outside that screen edge,
+            // move it back inside.
+            if(slimeBounds.Left < screenBounds.Left)
+            {
+                _slimePosition.X = screenBounds.Left;
+            }
+            else if(slimeBounds.Right > screenBounds.Right)
+            {
+                _slimePosition.X = screenBounds.Right - _slime.Width;
+            }
+
+            if(slimeBounds.Top < screenBounds.Top)
+            {
+                _slimePosition.Y = screenBounds.Top;
+            }
+            else if(slimeBounds.Bottom > screenBounds.Bottom)
+            {
+                _slimePosition.Y = screenBounds.Bottom - _slime.Height;
+            }
+
+            // Calculate the new position of the bat based on the velocity.
+            Vector2 newBatPosition = _batPosition + _batVelocity;
+
+            // Create a bounding circle for the bat.
+            Circle batBounds = new Circle(
+                (int)(newBatPosition.X + (_bat.Width * 0.5f)),
+                (int)(newBatPosition.Y + (_bat.Height * 0.5f)),
+                (int)(_bat.Width * 0.5f)
+            );
+
+            Vector2 normal = Vector2.Zero;
+
+            // Use distance based checks to determine if the bat is within the
+            // bounds of the game screen, and if it is outside that screen edge,
+            // reflect it about the screen edge normal.
+            if(batBounds.Left < screenBounds.Left)
+            {
+                normal.X = Vector2.UnitX.X;
+                newBatPosition.X = screenBounds.Left;
+            }
+            else if(batBounds.Right > screenBounds.Right)
+            {
+                normal.X = -Vector2.UnitX.X;
+                newBatPosition.X = screenBounds.Right - _bat.Width;
+            }
+
+            if(batBounds.Top < screenBounds.Top)
+            {
+                normal.Y = Vector2.UnitY.Y;
+                newBatPosition.Y = screenBounds.Top;
+            }
+            else if(batBounds.Bottom > screenBounds.Bottom)
+            {
+                normal.Y = -Vector2.UnitY.Y;
+                newBatPosition.Y = screenBounds.Bottom - _bat.Height;
+            }
+
+            // If the normal is anything but Vector2.Zero, this means the bat had
+            // moved outside the screen edge so we should reflect it about the
+            // normal.
+            if(normal != Vector2.Zero)
+            {
+                normal.Normalize();
+                _batVelocity = Vector2.Reflect(_batVelocity, normal);
+            }
+
+            _batPosition = newBatPosition;
+
+            if(slimeBounds.Intersects(batBounds))
+            {
+                // Divide the width and height of the screen into equal columns and
+                // rows based on the width and height of the bat.
+                int totalColumns = GraphicsDevice.PresentationParameters.BackBufferWidth / (int)_bat.Width;
+                int totalRows = GraphicsDevice.PresentationParameters.BackBufferHeight / (int)_bat.Height;
+
+                // Choose a random row and column based on the total number of each
+                int column = Random.Shared.Next(0, totalColumns);
+                int row = Random.Shared.Next(0, totalRows);
+
+                // Change the bat position by setting the x and y values equal to
+                // the column and row multiplied by the width and height.
+                _batPosition = new Vector2(column * _bat.Width, row * _bat.Height);
+
+                // Assign a new random velocity to the bat
+                AssignRandomBatVelocity();
+            }
+
             base.Update(gameTime);
         }
 
@@ -80,11 +189,25 @@ namespace DungeonSlime
             _slime.Draw(SpriteBatch, _slimePosition);
 
             //Draw bat sprite to the right of the slime
-            _bat.Draw(SpriteBatch, new Vector2(_slime.Width + 10, 0));
+            _bat.Draw(SpriteBatch, _batPosition);
 
             SpriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        private void AssignRandomBatVelocity()
+        {
+            // Generate a random angle.
+            float angle = (float)(Random.Shared.NextDouble() * Math.PI * 2);
+
+            // Convert angle to a direction vector.
+            float x = (float)Math.Cos(angle);
+            float y = (float)Math.Sin(angle);
+            Vector2 direction = new Vector2(x, y);
+
+            // Multiply the direction vector by the movement speed.
+            _batVelocity = direction * MOVEMENT_SPEED;
         }
 
         private void CheckKeyboardInput()
